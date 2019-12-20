99c846aefea76a2bc56f9b355e6664a0b97e868d
whytoj <56161523+whytoj@users.noreply.github.com>
2019-12-20 19:53:43 +0300

    Add files via upload

--- src/flags/flags_s.c
+++ src/flags/flags_s.c
@@ -6,7 +6,7 @@
 /*   By: lmelina <lmelina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 15:23:49 by lmelina           #+#    #+#             */
-/*   Updated: 2019/12/15 19:17:25 by lmelina          ###   ########.fr       */
+/*   Updated: 2019/12/20 19:45:52 by lmelina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,46 +19,77 @@ char	*ft_insert_s(va_list arg, int *flags)
 	char *res;
 
 	insert = va_arg(arg, char*);
-	if (!insert)
+	if (insert != NULL)
+		insert = ft_strdup(insert);
+//	printf("0 = \'%s\'\n", insert);
+
+	if (insert == NULL)
 	{
-		return (ft_strdup("(null)"));
+		if (flags[PRECISION] == -1)
+		{
+			res = ft_strdup("");
+		}
+		else
+			res = ft_strdup("(null)");
+		if (flags[PRECISION] < 6 && flags[PRECISION] != 0)
+			res[flags[PRECISION]] = '\0';
 	}
-	len = (int) ft_strlen(insert);
-	if (!(res = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	if (flags[PRECISION])
-		res = ft_strncpy(res, insert, flags[PRECISION]);
 	else
-		res = ft_strcpy(res, insert);
+	{
+//		printf("1 = \'%s\'\n", insert);
+		len = (int) ft_strlen(insert);
+		if (flags[PRECISION] < len && flags[PRECISION] != 0)
+			insert[flags[PRECISION]] = '\0';
+		if (flags[PRECISION] == -1)
+		{
+			res = ft_strdup("");
+		}
+		else
+			res = ft_strdup(insert);
+//		printf(" = \'%s\'\n", res);
+//		if (!(res = (char *)malloc(sizeof(char) * (len + 1))))
+//			return (NULL);
+//		if (flags[PRECISION])
+//			res = ft_strncpy(res, insert, flags[PRECISION]);
+//		else
+//			res = ft_strcpy(res, insert);
+	}
+
+//	printf("1 = \'%s\'\n", res);
+
+	//ft_putendl(res);
+
+//	printf("1 = \'%s\'\n", res);
+
+//	len = (int)ft_strlen(res);
+//	if (flags[PRECISION] > len)
+//		res = ft_strjoin_free(ft_str_spam("0", flags[PRECISION] - ft_strlen(res)), res);
 
 	if (flags[ZERO] == 1 && flags[PRECISION] == 0 && flags[MINUS] == 0)
 	{
 		len = (int)ft_strlen(res);
 		if (flags[PLUS] == 1 || flags[SPACE] == 1)
 			len ++;
-		res = ft_strjoin_free(ft_str_spam("0", flags[WIDTH] - len), res);
+		if (len < flags[WIDTH])
+			res = ft_strjoin_free(ft_str_spam("0", flags[WIDTH] - len), res);
 	}
 
-	if (flags[WIDTH] > 0 && (int)ft_strlen(res) < flags[WIDTH])
-	{
-		len = (int)ft_strlen(res);
-		res = ft_strjoin_free(ft_str_spam(" ", flags[WIDTH] - len), res);
-	}
+//	printf("2 = \'%s\'\n", res);
 
-	if (flags[MINUS] && flags[WIDTH])
+
+	if (flags[WIDTH] > 0 && (len = (int)ft_strlen(res)) < flags[WIDTH])
 	{
-		int pos = 0;
-		int j = 0;
-		while (res[j] != '\0')
+		if (flags[MINUS] == 1)
 		{
-			if (res[j] != ' ')
-			{
-				res[pos] = res[j];
-				res[j] = ' ';
-				pos++;
-			}
-			j++;
+			res = ft_strjoin_free(res, ft_str_spam(" ", flags[WIDTH] - len));
+		}
+		else
+		{
+			res = ft_strjoin_free(ft_str_spam(" ", flags[WIDTH] - len), res);
 		}
 	}
+
+//	printf("3 = \'%s\'\n", res);
+
 	return (res);
 }
